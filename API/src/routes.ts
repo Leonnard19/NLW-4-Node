e64379f61cf0560c7f676795e62d03e4ec770c62
Leonardo <srleon19@hotmail.com>
2021-02-24 19:27:05 -0300

    Tests using Jest

--- API/src/routes.ts
+++ API/src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import { SurveysController } from './controllers/SurveysController';
 import { UserControler } from './controllers/UserController';
 
 const router = Router();
 
 const userControler = new UserControler();
+const surveysControler = new SurveysController();
 
 router.post('/users', userControler.create);
+router.post('/surveys', surveysControler.create);
+router.get('/show', surveysControler.show);
 
 export { router };
